b2a5e1818053d5758955d80b188669cb121a3c52
Sheila Truong <sheilatruong96@gmail.com>
2018-01-17 13:37:40 -0800

    put all admin styling in one file

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var auth = require('./routes/auth');
+var dashboard = require('./routes/dashboard');
+
+var mongoose = require('mongoose');
+
+
 
 var app = express();
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log('CONNCTED TO MONGO');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +37,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/auth', auth);
+app.use('/admin', dashboard);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+// var mongoose = require('mongoose');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// var userSchema = mongoose.Schema({
+// 	email: String,
+// 	password: String,
+// });
+
+// var userModel = mongoose.model('users', userSchema);
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// router.post('/auth/register', function(req, res) {
+// 	var newUser = new userModel({
+// 		email: req.body.email,
+// 		password: req.body.password,
+// 	});
+
+// 	newUser.save(function(err, user) { 
+// 		if (err) return console.error(err);
+// 		res.redirect('/admin');
+// 	});
+
+// });
+
+
 
 module.exports = router;
